e258f853a0fb61bdfd25548768dd4a96d221d280
Skyler Austen <me@skylerausten.com>
2023-02-21 17:38:24 -0600

    2/21 Updates

--- Feb 21/rng.py
+++ Feb 21/rng.py
@@ -1,6 +1,11 @@
 # Step 1: Generate a random integer between 1 and 50.
+
 # Step 2: Prompt the user to guess the number.
+
 # Step 3: Loop until the user guesses the correct number.
+
 # Step 3.1: Check if the guess is too high or too low.
+
 # Step 3.2: Prompt the user to guess again if needed.
+
 # Step 4: Print a message when the user guesses correctly.
