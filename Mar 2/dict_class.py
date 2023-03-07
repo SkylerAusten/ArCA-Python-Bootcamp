68b31320c969834c636263a8d3aecc37a0b2595c
Skyler Austen <me@skylerausten.com>
2023-03-07 17:18:52 -0600

    Mar 7 Class Materials

--- Mar 2/dict_class.py
+++ Mar 2/dict_class.py
@@ -1,12 +1,13 @@
-# TODO: The thing.
-dictionary = {'name': 'Kaitlyn',
-              "last_initial": "s"}
-dictionary["favorite_color"] = "yellow"
-dictionary["day_of_birth"] = 27
-dictionary["favorite_foods"] = ["Pizza", "Chicken Nuggets", "Mac & Cheese"]
-dictionary["favorite_color"] = "blue"
-dictionary[0] = 0
-dictionary[0.1] = 0.1
-print()
-print(dictionary["favorite_color"])
-print(dictionary[0.1])
+import random
+
+list = []
+
+for i in range(5):
+    temp_dict = {}
+    temp_dict["id"] = i
+    temp_dict["val"] = random.randint(0, 10)
+    list.append(temp_dict)
+
+print(list)
+
+print(list[])
